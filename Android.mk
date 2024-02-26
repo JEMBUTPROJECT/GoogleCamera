8778fe3c8e94f58c5082ee8bae5aa70cf5108e31
FebriCahyaa <febricahya12345@gmail.com>
2024-02-26 14:05:42 +0000

    GoogleCamera Nikita v2.0

---
+++ Android.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2fd447321697e26ee2587ea0f77cfb6b91f29646f3cd29574ea061a20135375
+size 74
---
+++ GoogleCamera-vendor.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7525142b0a0db1a1815cc37c56cfd4a6a5d49805d9a4c12526e1771a982b1d31
+size 111
